d0b3d457e810dced1d22b86eedb85e1994586625
emrekrci <kkirci.emre@gmail.com>
2019-10-22 00:08:32 +0300

    Add project files.

---
+++ MLProject.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+namespace MLProject.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Core");
+        }
+    }
+}
